76b088e730c3b861fd02e823e9f819fc34fca8e9
anprartt <anprartt@gmail.com>
2023-12-23 23:23:38 +0300

    added prettier and husky (all files will be formatted in the next commit)

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    maxLength: 110,
+};
+
+module.exports = config;
